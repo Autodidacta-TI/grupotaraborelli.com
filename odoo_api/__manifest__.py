b167ef7649642febe05a0d089655d0c32273ca7b
Ignacio Buioli <ibuioli@gmail.com>
2022-09-07 15:35:39 -0300

    testing api

---
+++ odoo_api/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "odoo_api",
+
+    'summary': """
+        Odoo API XMLRPC Style custom for Taraborelli""",
+
+    'description': """
+        Odoo API XMLRPC Style for Taraborelli
+    """,
+
+    'author': "Codize",
+    'website': "https://www.codize.ar",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['base'],
+
+    'data': [],
+
+    'demo': [],
+}
